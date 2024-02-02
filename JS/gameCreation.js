461dd63e36bf9408bb15978a65429c2c3b2cc340
worldOfVerdure <perspicaciousdotard@gmail.com>
2024-02-02 05:55:50 -0600

    Add minor bug fix.

--- JS/gameCreation.js
+++ JS/gameCreation.js
@@ -1,4 +1,4 @@
-export function setGame(event) {
+export function setGame() {
   let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
   let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
   const myMain = document.querySelector("main");
