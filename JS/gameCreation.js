ea93ee7d99caac71e41028d202f8e9bf08369fa8
worldOfVerdure <perspicaciousdotard@gmail.com>
2024-02-15 20:33:16 -0600

    Code doesn't work, trying to select optgroup.

--- JS/gameCreation.js
+++ JS/gameCreation.js
@@ -1,6 +1,29 @@
-export function setGame() {
-  let vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
-  let vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
-  const myMain = document.querySelector("main");
-  myMain.innerText = `The vw is ${vw} and the vh is ${vh}`;
+export function setGame(event) {
+  const vw = Math.max(document.documentElement.clientWidth || 0, window.innerWidth || 0);
+  const vh = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+  let numOfTiles;
+  
+  if ((vw <= 600) || (vh <= 700))
+    numOfTiles = 14;
+
+  else if ((vw > 600 && vw <= 1000) || (vh > 700 && vh <= 1000))
+    numOfTiles = 22;
+
+  else if ((vw > 1000) || (vh > 1000))
+    numOfTiles = 34;
+
+  else
+    numOfTiles = undefined;
+
+  if (numOfTiles !== undefined) {
+    const optgroupSelection = event.target.parentElement.tagName;
+    alert(optgroupSelection);
+  }
 }
+
+// export function setGame(event) {
+//   const optgroupSelection = event.target.parentElement; // get the optgroup element
+//   alert(optgroupSelection.label); // show the optgroup label
+//   // do something with the optgroup element, such as changing its style
+//   optgroupSelection.style.backgroundColor = "yellow";
+// }
